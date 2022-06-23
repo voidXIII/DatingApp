4c3a60b414b2cf6a6bb174601f690aa53ba161c2
voidXIIIth <ion.vasilasco.1997@gmail.com>
2022-06-23 18:25:30 +0300

    Member Edit, Persisting changes in API

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemeberDetailComponent } from './members/memeber-detail/memeber-detail.component';
 import { MemeberListComponent } from './members/memeber-list/memeber-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -19,6 +21,7 @@ const routes: Routes = [
         children: [
             {path: 'members', component: MemeberListComponent},
             {path: 'members/:username', component: MemeberDetailComponent},
+            {path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
             {path: 'lists', component: ListsComponent},
             {path: 'messages', component: MessagesComponent},
         ]
